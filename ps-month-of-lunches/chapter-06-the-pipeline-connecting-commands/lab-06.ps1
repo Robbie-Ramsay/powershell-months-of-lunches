25d210d91674769f11a15b591f80dc7caef66754
Robbie Ramsay <ramsay.rga@gmail.com>
2026-01-12 15:03:29 +0000

    Half of lab completed

--- ps-month-of-lunches/chapter-06-the-pipeline-connecting-commands/lab-06.ps1
+++ ps-month-of-lunches/chapter-06-the-pipeline-connecting-commands/lab-06.ps1
@@ -33,3 +33,21 @@ Get-Process -Id $PID | ConvertTo-Html | Out-File website.html
 Get-Process | Stop-Process –WhatIf
 
 # Start of lab
+
+# Create two similar, but different, text files. Try comparing them by using Compare-Object.
+Set-Location -Path C:\Code\powershell-months-of-lunches\ps-month-of-lunches\chapter-06-the-pipeline-connecting-commands
+Help New-Item -Full
+New-Item -Path . -Name "comparefile1.txt" -ItemType "File" -Value "123456"
+New-Item -Path . -Name "comparefile2.txt" -ItemType "File" -Value "654321"
+Help Compare-Object -Full
+Compare-Object -ReferenceObject (Get-Content ./comparefile1.txt) -DifferenceObject (Get-Content ./comparefile2.txt)
+
+# What happens if you run Get-Command | Export-CSV commands.CSV | Out-File from the console? Why does that happen?
+Get-Command | Export-CSV commands.CSV | Out-File
+# An empty file is created, this is becuase Export-CSV is not designed to push anything down the pipeline so Out-File has nothing for the value of the file
+
+# Apart from getting one or more jobs and piping them to Stop-Job, what other means does Stop-Job provide for you to specify the job or jobs you want to stop?
+# Is it possible to stop a job without using Get-Job at all?
+Help Get-Job
+Help Stop-Job
+# You can use the -Name and -Job paramaters to specify specific jobs, you can also use Ctr+C to stop the current job.
