4482a14e893269e7c1c44f47a0645d58b60ab7e3
Robbie Ramsay <ramsay.rga@gmail.com>
2026-01-13 12:37:54 +0000

    Completed reading chapter 7

--- ps-month-of-lunches/chapter-07-adding-commands/lab-07.ps1
+++ ps-month-of-lunches/chapter-07-adding-commands/lab-07.ps1
@@ -1,1 +1,9 @@
 # Lab for Chapter 7: Adding commands
+
+(Get-Content Env:/PSModulePath) -split ':'
+
+# Cleaned up my DISGUSTING file paths holy moly
+
+# Finished reading chapter
+
+# Start of lab --------------------------
